3d78dda2696ba817ce2f30d9c030191f9b449941
Eric Chan <eric88ordinary@gmail.com>
2022-02-03 11:28:42 +0800

    add amplify

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+import awsconfig from './src/aws-exports';
+import Home from './components/Home';
+import '@aws-amplify/ui-react/styles.css';
 
-export default function App() {
+Amplify.configure(awsconfig);
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Home />
     </View>
   );
+}
+
+export default function main() {
+  return (
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
+  )
 }
 
 const styles = StyleSheet.create({
